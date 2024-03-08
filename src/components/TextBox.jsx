9458d2e9e74a4710d9187910afaeeef3da287af1
coder-saim <saim.cse.du.744@gmail.com>
2024-03-08 23:42:00 +0600

    text box modified

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const TextBox = () => {
   const [textBoxes, setTextBoxes] = useState([{ id: 1, value: "" }]);
@@ -59,8 +60,11 @@ const TextBox = () => {
         Add
       </button>
       <div className="mt-4">Sum: {calculateSum()}</div>
+      <button className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link to="/">Back</Link>
+      </button>
     </div>
   );
-}
+};
 
 export default TextBox;
