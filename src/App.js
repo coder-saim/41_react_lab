5d58cfc67409752afdaf944887255ae8f2524a4c
coder-saim <saim.cse.du.744@gmail.com>
2024-03-08 23:14:09 +0600

    project started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TaskManager from "./components/TaskManager";
+import TextBox from "./components/TextBox";
+
+function App() {
+  return (
+    <>
+      <TextBox />
+      <TaskManager/>
+    </>
+  );
+}
+
+export default App;
