3a644b3646f176519f7d90a5693240ca98f289b4
coder-saim <saim.cse.du.744@gmail.com>
2024-03-08 23:42:56 +0600

    task done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import HomePage from "./components/HomePage";
 import TaskManager from "./components/TaskManager";
 import TextBox from "./components/TextBox";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <TextBox />
-      <TaskManager/>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/textbox" element={<TextBox />} />
+      <Route path="/taskmanager" element={<TaskManager />} />
+    </Routes>
   );
 }
 
