371aab455884029d8aa030ab22e09bb0d912eb53
coder-saim <saim.cse.du.744@gmail.com>
2024-03-27 23:23:03 +0600

    register route added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import HomePage from "./components/HomePage";
+import Register from "./components/Register";
 import TaskManager from "./components/TaskManager";
 import TextBox from "./components/TextBox";
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -6,7 +7,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/homepage" element={<HomePage />} />
+      <Route path="/" element={<Register />} />
       <Route path="/textbox" element={<TextBox />} />
       <Route path="/taskmanager" element={<TaskManager />} />
     </Routes>
